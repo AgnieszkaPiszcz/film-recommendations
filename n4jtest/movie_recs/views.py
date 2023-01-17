05b021bd05376843a580c911aeae757671587986
Agnieszka Piszcz <agnieszkaapiszcz@gmail.com>
2023-01-17 12:02:06 +0100

    first commit

---
+++ n4jtest/movie_recs/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from n4jtest import N4jConnection
+from neo4j import Record
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    conn = N4jConnection.N4jConnection("bolt://localhost:7687", "neo4j", "adminadmin")
+    res =  conn.query("MATCH p=()-[r:RATED]->() RETURN p LIMIT 5")
+    return HttpResponse(len(res))
