57cf505a7d0522298e7b135de8f0f0de05ab20bb
Agnieszka Piszcz <agnieszkaapiszcz@gmail.com>
2023-01-22 16:34:44 +0100

    views

--- n4jtest/movie_recs/views.py
+++ n4jtest/movie_recs/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from n4jtest import N4jConnection
 from neo4j import Record
+from pandas import DataFrame
+from django.conf import settings
+from movie_recs.repository import *
+from movie_recs.search import search
+from pprint import pprint
 
 # Create your views here.
 
@@ -8,6 +13,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    conn = N4jConnection.N4jConnection("bolt://localhost:7687", "neo4j", "adminadmin")
-    res =  conn.query("MATCH p=()-[r:RATED]->() RETURN p LIMIT 5")
-    return HttpResponse(str(res))
+    res = get_ratings()
+    movies = get_movies()
+    s = search(movies, "toy story")
+    print(type(res))
+    print(res.to_json(orient='values'))
+    res.to_json()
+    
+    return HttpResponse(res.to_string())
+
+# id usera i jakie filmy ocenił na jakie oceny
+ 
